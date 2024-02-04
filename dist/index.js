4c9c1226009e75cd9ba81ccdb7ed8aebeb801192
hilomafrancisoliver <hilomafrancisoliver@gmail.com>
2024-02-04 20:54:44 +0800

    Continues design for hamburger

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 menu = (e) => {
-    let selectMenu = document.querySelector('#mobile-menu');
-    e.name === 'menu' ? e.name = 'close' : e.name = 'menu'
-   }
+ let selectMenu = document.querySelector('#mobile-menu');
+ e.name === 'menu' ? (e.name = 'close', selectMenu.style.opacity = 100) : (e.name = 'menu', selectMenu.style.opacity = 0)
+}
