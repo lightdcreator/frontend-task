e190d3a16678f5451cc5d48d5ea2e94d85f139da
hilomafrancisoliver <hilomafrancisoliver@gmail.com>
2024-03-21 23:27:43 +0800

    image for main

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-menu = (e) => {
- let selectMenu = document.querySelector('#mobile-menu');
- e.name === 'menu' ? (e.name = 'close', selectMenu.style.opacity = 100) : (e.name = 'menu', selectMenu.style.opacity = 0)
-}
